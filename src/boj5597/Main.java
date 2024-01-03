3339e5fb1783ff289fb20ce5b7c382048f029039
LEE-Kyungjae <ze2@kakao.com>
2024-01-03 13:59:06 +0900

    add somthing

---
+++ src/boj5597/Main.java
@@ -0,0 +1,18 @@
+package boj5597;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		//Scanner sc = new Scanner(System.in);
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		String line[] = in.readLine().split(" ");
+	}
+}
