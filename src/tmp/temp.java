d1dc649297eed41b35db820ef3ad95a27480f904
LEE-Kyungjae <ze2@kakao.com>
2024-01-12 14:04:01 +0900

    a

--- src/tmp/temp.java
+++ src/tmp/temp.java
@@ -1,19 +1,19 @@
-//boj
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb;
-		int n = Integer.parseInt(in.readLine());
-	}
-}
-
-//in
-in.readLine()
-
-//ip
-Integer.parseInt();
+////boj
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;
+//		StringBuilder sb;
+//		int n = Integer.parseInt(in.readLine());
+//	}
+//}
+//
+////in
+//in.readLine()
+//
+////ip
+//Integer.parseInt();
