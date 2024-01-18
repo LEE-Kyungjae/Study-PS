2a8ce5cc35810ea313f414120f35f008669cb396
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:29:40 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 428 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		for (int i = 0; i < n; i++) {
+			for (int j = n - i - 1; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < n-1; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2*(n)-2*i-3; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
