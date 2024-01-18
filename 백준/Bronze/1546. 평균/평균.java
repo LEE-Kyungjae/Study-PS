811cd9db57f705eafac5baa23dbcf4f9a6513c28
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:24:56 +0900

    [Bronze I] Title: 평균, Time: 84 ms, Memory: 11856 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n= Integer.parseInt(in.readLine());
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		double max = 0;
+		double[] arr = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		
+			if(max<arr[i]) max=arr[i];
+		}
+		for (int i = 0; i < n; i++) {
+			arr[i]=arr[i]/max*100;
+		}
+		double sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum+= arr[i];
+		}
+		
+		System.out.println(sum/n);
+		
+	}
+}
