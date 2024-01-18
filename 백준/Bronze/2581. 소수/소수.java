ae4968b5a5612ba1c3362987d775fc9d4d9583a8
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:25:17 +0900

    [Bronze II] Title: 소수, Time: 112 ms, Memory: 11688 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(in.readLine());
+		int N = Integer.parseInt(in.readLine());
+		int min = N;
+		// M<=N
+		int sum = 0;
+		out: for (int i = M; i <= N; i++) {
+			if (i == 1) {
+				continue;
+			}
+			for (int j = 2; j <= (i + 1) / 2; j++) {
+				if (i % j == 0) {
+					continue out;
+				}
+			}
+			sum += i;
+			if (min > i) {
+				min = i;
+			}
+		}
+		if (sum != 0) {
+			System.out.println(sum);
+			System.out.println(min);
+		} else {
+			System.out.println(-1);
+
+		}
+	}
+}
