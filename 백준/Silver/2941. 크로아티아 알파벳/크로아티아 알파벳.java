0408541d222d911e95962441778a04eb3c009f1f
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:24:43 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String str = in.readLine();
+		String[] prefix = { "c=", "c-", "d-", "lj", "dz=", "nj", "s=", "z=" };
+		int len = str.length();
+		int cnt = 0;
+		String temp = "";
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < prefix.length; j++) {
+				if (str.charAt(i) == prefix[j].charAt(0)) {
+					temp = str.substring(i, str.length());
+					if (temp.startsWith(prefix[j])) {
+						cnt += prefix[j].length() - 1;
+						i=i+prefix[j].length()-1;
+					}
+				}
+			}
+		}
+		System.out.println(len - cnt);
+	}
+
+}
