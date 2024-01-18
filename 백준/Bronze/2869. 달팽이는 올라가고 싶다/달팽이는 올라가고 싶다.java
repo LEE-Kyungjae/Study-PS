12753cfac35e7ea2bb2da7541785eb4cbf477897
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:27:36 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		Long V = Long.parseLong(st.nextToken());
+		Long last = V - A;
+		Long midcnt = last / (A - B);
+		Long left = last % (A - B);
+		// last = (midcnt * (A-B)) + left;
+		Long midmiter = midcnt * (A - B);
+		while (true) {
+			midmiter += A;
+			midcnt++;
+			if (V <= midmiter) {
+				break;
+			}
+			midmiter -= B;
+
+		}
+		System.out.println(midcnt);
+	}
+}
