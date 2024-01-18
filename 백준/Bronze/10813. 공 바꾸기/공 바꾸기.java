a34fc9c6411983c91ab115e33891666392b6bf5b
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-18 23:28:15 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 13088 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] arr = new int[n];
+		int i, j;
+		int tmp;
+		for (int x = 0; x < n; x++) {
+			arr[x]=x+1;
+		}
+		for (int k = 0; k < m; k++) {
+			i = sc.nextInt();
+			j = sc.nextInt();
+			tmp=arr[i-1];
+			arr[i-1]=arr[j-1];
+			arr[j-1]=tmp;
+		}
+		for (int k = 0; k < n; k++) {
+			System.out.println(arr[k]);
+		}
+	}
+}
