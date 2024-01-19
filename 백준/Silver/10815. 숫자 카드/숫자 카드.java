fdf21064412336ae07de1e2a8e6461f4d2f03811
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-01-19 15:25:13 +0900

    [Silver V] Title: 숫자 카드, Time: 868 ms, Memory: 175088 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int M = Integer.parseInt(br.readLine());
+		Set<Integer> set = new HashSet<Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			set.add(Integer.parseInt(st.nextToken()));
+		}
+		int M2 = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M2; i++) {
+			if (set.contains(Integer.parseInt(st.nextToken()))) {
+				sb.append(1).append(" ");
+			} else {
+				sb.append(0).append(" ");
+			}
+		}
+		System.out.println(sb);
+	}
+}
