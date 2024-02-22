738fe449b16e509312ac31a7b4428a65074a796d
Kyungjae LEE <108649270+LEE-Kyungjae@users.noreply.github.com>
2024-02-22 15:51:53 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12216 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	final static int Red = 0;
+	final static int Green = 1;
+	final static int Blue = 2;
+	static int[][] cost;
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		cost = new int[N][3];
+		dp = new int[N][3];
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			cost[i][Red] = Integer.parseInt(st.nextToken());
+			cost[i][Green] = Integer.parseInt(st.nextToken());
+			cost[i][Blue] = Integer.parseInt(st.nextToken());
+		}
+
+		dp[0][Red] = cost[0][Red];
+		dp[0][Green] = cost[0][Green];
+		dp[0][Blue] = cost[0][Blue];
+		System.out
+				.println(Math.min(Paint_cost(N - 1, Red), Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue))));
+	}
+
+	static int Paint_cost(int N, int color) {
+		if (dp[N][color] == 0) {
+			if (color == Red) {
+				dp[N][Red] = Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue)) + cost[N][Red];
+			} else if (color == Green) {
+				dp[N][Green] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Blue)) + cost[N][Green];
+			} else {
+				dp[N][Blue] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Green)) + cost[N][Blue];
+			}
+		}
+		return dp[N][color];
+	}
+}
